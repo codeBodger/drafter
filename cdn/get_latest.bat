b41581b404f72be930a17dec67dd4d26340859bd
codeBodger <codeBodger@users.noreply.github.com>
2025-07-30 00:31:41 +0000

    deploy: 19610f479d216049bee524f8778428ddd032b0d2

--- cdn/get_latest.bat
+++
@@ -1,4 +0,0 @@
-cp ../drafter/dist-js/skulpt-drafter.js skulpt/
-cp ../../blockpy-edu/skulpt/dist/skulpt.js skulpt/
-cp ../../blockpy-edu/skulpt/dist/skulpt-stdlib.js skulpt/
-cp ../../blockpy-edu/skulpt/dist/skulpt.js.map skulpt/
